d4c9ce74f08fd5a92f772fcc1fdcde69caff8fe2
hsalamat <2hooman.salamat@gmail.com>
2025-05-26 20:02:07 -0400

    update

---
+++ SportsAnalytics/src/loadsofascoredata.ts
@@ -0,0 +1,29 @@
+console.log("hello typescript");
+    const xhttpr = new XMLHttpRequest();
+    xhttpr.open('GET', 'https://sofascore.p.rapidapi.com/teams/get-next-matches?teamId=3419', true);
+ //   xhttpr.open('GET', 'https://sofascore.p.rapidapi.com/teams/detail?teamId=3419', true);
+//xhttpr.open('GET', 'https://sofascore.p.rapidapi.com/players/detail?playerId=846971', true);
+//xhttpr.open('GET', 'https://sofascore.p.rapidapi.com/search?q=stakusic&type=all&page=0', true);
+//xhttpr.open('GET', 'https://sofascore.p.rapidapi.com/search?q=siakam&type=all&page=0', true);
+xhttpr.setRequestHeader('Content-Type', 'application/json');
+xhttpr.setRequestHeader('x-rapidapi-host', 'sofascore.p.rapidapi.com');
+xhttpr.setRequestHeader('x-rapidapi-key', '82f34d4facmsh62729fce9bea6d8p197e46jsn4612f69a776a');
+xhttpr.send();
+
+xhttpr.onload = ()=> {
+  if (xhttpr.status === 200) {
+      const response = JSON.parse(xhttpr.response);
+      // Process the response data here
+      const pretty = JSON.stringify(response, null, 2);
+      console.log(pretty);
+
+      const element = document.getElementById('json');
+
+    if (element) {
+        element.textContent = JSON.stringify(response, undefined, 2);
+    }
+  } else {
+      // Handle error
+      console.log("error");
+  }
+};
