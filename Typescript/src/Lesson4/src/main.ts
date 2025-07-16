527c408dd5f20001f5d5f0e4438634321946f217
hsalamat <2hooman.salamat@gmail.com>
2025-07-16 17:43:29 -0400

    Cone with lighting

--- Typescript/src/Lesson4/src/main.ts
+++ Typescript/src/Lesson4/src/main.ts
@@ -1,160 +1,78 @@
-//Arrays
-let stringArr = ['one', 'two', 'tree']      //string type
-let guitars = ['Strat', 'Les Paul', 5150]  //union type
-let mixedData = ['EVH', 1984, true]
 
-stringArr[0] = 'John'
-stringArr.push('Jane')
+type stringOrNumber = string | number;
 
-//guitars is a union type
-guitars[0] = 1984
-guitars.unshift('Jim')
-console.log(guitars)
+//you cannot do this with interfaces!
+type UserId = stringOrNumber
 
-let test = []
-let bands: string[] = []
-bands.push('Van Halen')
+type stringOrNumberArray = (string | number)[]
 
-//Tuple
-let myTuple: [string, number, boolean] = ['Hooman', 42, true]
-let mixed = ['John', 1, false]
-
-//Objects
-let myObj: object
-myObj = []
-console.log(typeof myObj)
-
-myObj = bands
-myObj = {}
-
-const exampleObj = {
-    prop1: 'Dave',
-    prop2: true
-}
-
-exampleObj.prop1 = 'John'
-exampleObj.prop2 = false
-
+//Type Aliases
 type Guitarist = {
-    name: string,
-    active?: boolean,
-    albums: (string | number)[]
+    name?: string,
+    active: boolean,
+    albums: stringOrNumberArray
 }
 
-let evh: Guitarist = { 
-    name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812']
+//Literal types
+let myName: 'Hooman'
+let userName: 'Dave' | 'John' | 'Amy'
+userName = 'Amy'
+
+//functions
+const add = (a: number, b: number) => {
+    return a + b;
 }
 
-let jp: Guitarist = {
-    name: 'Jimmy',
-    albums: ['I', 'II', 'IV']
+const logMsg = (message: any): void => {
+    console.log(message)
 }
 
-evh = jp
+logMsg('Hello')
+logMsg(add(2, 3))
 
-const greetGuitarist = (guitarist:Guitarist) => {
-    return `Hello ${guitarist.name}!`;
+//you can also use "function" instead of =>
+let subtract = function (c: number, d: number): number {
+    return c - d
 }
 
-console.log(greetGuitarist(jp))
+type mathFunction = (a: number, b: number) => number
 
-const greetGuitaristoptional = (guitarist:Guitarist) => {
-    return `Hello ${guitarist.active}!`;
+
+let multiply: mathFunction = function (c, d) {
+    return c * d
 }
 
-console.log(greetGuitaristoptional(jp))
+logMsg(multiply(2, 2))
 
-//You can also use interface instead of type
-interface IGuitarist  {
-    name: string,
-    active?: boolean,
-    albums: (string | number)[]
+//You can also do the same thing with interfaces
+
+interface ImathFunction {
+    (a: number, b: number): number
 }
 
-let evhi: Guitarist = { 
-    name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812']
+let divide: ImathFunction = function (c, d) {
+    return c / d
 }
 
-let jpi: Guitarist = {
-    name: 'Jimmy',
-    albums: ['I', 'II', 'IV']
-}
+logMsg(divide(4, 2))
 
-evhi = jpi
-
-const greetGuitaristi = (guitarist:IGuitarist) => {
-    return `Hello ${guitarist.name}!`;
-}
-
-console.log(greetGuitaristi(jpi))
-
-// Type: Defining Unions, Intersections, and Aliases
-// With Type, we have the flexibility to create aliases for existing types, define unions or intersections of types, and even create complex data structures. It is particularly useful when dealing with scenarios that require combining multiple types or reusing them in various parts of the codebase.
-// Let's take an example to illustrate the power of Type:
-
-type Pet = {
-    name: string;
-    age: number;
-};
-
-type Dog = Pet & {
-    breed: string;
-};
-
-type Cat = Pet & {
-    color: string;
-};
-
-function printPetInfo(pet: Pet) {
-    console.log(`Name: ${pet.name}, Age: ${pet.age}`);
-}
-
-function printDogInfo(dog: Dog) {
-    console.log(`Name: ${dog.name}, Age: ${dog.age}, Breed: ${dog.breed}`);
-}
-//In the above example, we define a Pet type and then create two additional types, Dog and Cat, by combining it with additional properties specific to each. This makes our code more organized, maintainable, and less prone to errors.
-
-//Interface: Extending Objects and Classes
-//On the other hand, Interface in TypeScript is primarily used for extending object shapes and classes. It allows us to specify the structure that an object must adhere to, providing a clear contract for the code.
-
-//Let's demonstrate the use of Interface with an example:
-
-interface Shape {
-    name: string;
-    area(): number;
-}
-
-class Circle implements Shape {
-    constructor(public radius: number) {}
-
-    name = "Circle";
-
-    area() {
-        return Math.PI * this.radius * this.radius;
+//optional parameter should at the end!!!
+const addAll = (a: number, b: number, c?: number):
+    number => {
+    if (typeof c !== 'undefined') {
+        return a + b + c
     }
+    return a + b;
 }
 
-//Enums
-enum Grade {
-    U,
-    D,
-    C,
-    B,
-    A
+//default
+const sumAll = (a: number= 10, b: number, c: number = 2):
+    number => {
+    return a + b + c;
 }
 
-console.log(Grade.U)  //0
-
-enum Grade2 {
-    U = 1,
-    D,
-    C,
-    B,
-    A
-}
-
-console.log(Grade.U)  //1
+logMsg(addAll(2,3,4))
+logMsg(addAll(2,3))
+logMsg(sumAll(2,3,4))
+logMsg(sumAll(2,3))
+logMsg(sumAll(undefined,3))  //10+3+2 = 15
---
+++ WebGPU/gpu021/README.md
@@ -0,0 +1,16 @@
+# Step-by-Step WebGPU Graphics Programming (21) 
+## Cylinder with Lighting
+
+### npm install
+
+### Update vertex_data.ts
+
+### Update index.html under dist folder
+
+### Update main.ts
+
+### npm run prod
+
+### Open index.html using live server
+
+You can use left mouse button to rotate, right mouse button to pan, and middle mouse to zoom in/out.
