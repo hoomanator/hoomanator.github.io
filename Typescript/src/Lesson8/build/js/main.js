b44e4c579a7bc598c1b94c7e942f6b65b09829eb
hsalamat <2hooman.salamat@gmail.com>
2025-08-15 08:37:31 -0400

    lesson 8

--- Typescript/src/Lesson8/build/js/main.js
+++ Typescript/src/Lesson8/build/js/main.js
@@ -1,69 +1,38 @@
 "use strict";
-//Index Signatures
-//In TypeScript, an index signature defines the types 
-// for properties within an object where the exact property names 
-// are not known ahead of time. It allows for defining a consistent type 
-// for all values associated with dynamically named keys.
-const todaysTransactions = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50
+//The following function only works with string
+const stringEcho = (arg) => arg;
+console.log(stringEcho('Hooman'));
+//How to make a generic function
+const echo = (arg) => arg;
+console.log(echo('Hooman'));
+console.log(echo(1234));
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
 };
-console.log(todaysTransactions.Pizza);
-console.log(todaysTransactions['Pizza']);
-let prop = 'Pizza';
-const todaysTransactions2 = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50
-};
-console.log(todaysTransactions2[prop]);
-const todaysNet = (transactions) => {
-    let total = 0;
-    for (const transaction in transactions) {
-        total += transactions[transaction];
+console.log(isObj(true));
+console.log(isObj('John'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'John' }));
+console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
     }
-    return total;
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    //!! flip it around twice to make it true and false
+    return { arg, is: !!arg };
 };
-console.log(todaysNet(todaysTransactions2));
-//if you remove Job, it will give you an error
-//but you can add a new property
-const todaysTransactions3 = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50,
-    Salary: 400
-};
-console.log(todaysNet(todaysTransactions3));
-const student = {
-    name: "Doug",
-    GPA: 3.5,
-    classes: [100, 200]
-};
-//console.log(student.test)  //this is acceptable as a by product of index signature!
-for (const key in student) {
-    console.log(`${key}: ${student[key]}`);
-}
-//if we didn't have index signature, we have to use "assertion"
-for (const key in student) {
-    console.log(`${key}: ${student[key]}`);
-}
-//if you are not sure what Interface student comes from, you can also use typeof keyword
-Object.keys(student).map(key => {
-    console.log(student[key]);
-});
-//function
-const logStudentKey = (student, key) => {
-    console.log(`Student ${key}: ${student[key]}`);
-};
-//call the function
-logStudentKey(student, 'GPA');
-const monthlyIncomes = {
-    salary: 500,
-    bonus: 100,
-    sidehustle: 250
-};
-for (const revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[revenue]);
-}
-//3:01:54
+console.log(isTrue(true));
+console.log(isTrue('John'));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue({ name: 'John' }));
+console.log(isTrue(null));
+console.log(isTrue(NaN));
+console.log(isTrue(0));
+console.log(isTrue({}));
+console.log(isTrue(0));
+console.log(isTrue([]));
+console.log(isTrue(undefined));
+// 3:12:28
--- Typescript/src/Lesson8/src/main.ts
+++ Typescript/src/Lesson8/src/main.ts
@@ -1,133 +1,46 @@
-//Index Signatures
-//In TypeScript, an index signature defines the types 
-// for properties within an object where the exact property names 
-// are not known ahead of time. It allows for defining a consistent type 
-// for all values associated with dynamically named keys.
 
-interface TransactionObj {
-    Pizza: number,
-    Books: number,
-    Job: number
+//The following function only works with string
+const stringEcho =(arg: string): string => arg
+console.log(stringEcho('Hooman'))
+
+//How to make a generic function
+const echo =<T>(arg: T): T => arg
+console.log(echo('Hooman'))
+console.log(echo(1234))
+
+const isObj = <T>(arg:T): boolean => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null)
 }
 
-const todaysTransactions: TransactionObj = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50
-}
+console.log(isObj(true))
+console.log(isObj('John'))
+console.log(isObj([1,2,3]))
+console.log(isObj({name: 'John'}))
+console.log(isObj(null))
 
-console.log(todaysTransactions.Pizza)
-console.log(todaysTransactions['Pizza'])
-
-let prop: string = 'Pizza'
-//console.log(todaysTransactions[prop])  Typescript doesn't like this
-//Because we have not created index signature
-
-interface TransactionObj2 {
-    readonly [index: string] : number
-}
-
-const todaysTransactions2: TransactionObj2 = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50
-}
-
-console.log(todaysTransactions2[prop]) 
-
-const todaysNet = (transactions: TransactionObj2):
-number => {
-    let total =0
-    for (const transaction in transactions) {
-        total += transactions[transaction] 
+const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
+    if(Array.isArray(arg) && !arg.length) {
+        return {arg, is: false}
     }
-    return total
+    if(isObj(arg) && !Object.keys(arg as keyof T).length)
+    {
+        return { arg, is: false}
+    }
+    //!! flip it around twice to make it true and false
+    return { arg, is: !!arg}
 }
 
-console.log(todaysNet(todaysTransactions2))
 
-//todaysTransactions2.Pizza = 40  because we made it readonly
+console.log(isTrue(true))
+console.log(isTrue('John'))
+console.log(isTrue([1,2,3]))
+console.log(isTrue({name: 'John'}))
+console.log(isTrue(null))
+console.log(isTrue(NaN))
+console.log(isTrue(0))
+console.log(isTrue({}))
+console.log(isTrue(0))
+console.log(isTrue([]))
+console.log(isTrue(undefined))
 
-//2:47:00
-//you also can control the minimum properties that goes to an object.
-interface TransactionObj3 {
-    readonly [index: string] : number
-    Pizza: number,
-    Books: number,
-    Job: number
-}
-
-//if you remove Job, it will give you an error
-//but you can add a new property
-const todaysTransactions3: TransactionObj3 = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50,
-    Salary: 400
-}
-
-console.log(todaysNet(todaysTransactions3))
-
-//////////////////////////////////////
-interface Student{
-    [key:string]: string | number | number[] | undefined
-    name: string,
-    GPA: number,
-    classes?: number[]
-}
-
-const student: Student = {
-name: "Doug",
-GPA: 3.5,
-classes: [100,200]
-};
-
-//console.log(student.test)  //this is acceptable as a by product of index signature!
-
-for(const key in student){
-console.log(`${key}: ${student[key]}`)
-}
-
-//if we didn't have index signature, we have to use "assertion"
-for(const key in student){
-console.log(`${key}: ${student[key as keyof Student]}`)
-}
-
-//if you are not sure what Interface student comes from, you can also use typeof keyword
-Object.keys(student).map(key => {
-    console.log(student[key as keyof typeof student])
-})
-
-//function
-const logStudentKey = (student: Student, key: keyof Student): void =>
-{
-    console.log(`Student ${key}: ${student[key]}`)
-}
-
-//call the function
-logStudentKey(student, 'GPA')
-
-////////////////////////////////
-
-interface Incomes {
-    [key:string]: number
-}
-
-type Streams = 'salary' | 'bonus' | 'sidehustle'
-
-//with Incomes interface, you cannot have literal type!  [key:'salary']: number
-//with Incomes2, we allow literal types as well.
-type Incomes2 = Record<Streams, number>
-
-const monthlyIncomes: Incomes2 = {
-    salary:500,
-    bonus:100,
-    sidehustle: 250
-}
-
-for (const revenue in monthlyIncomes)
-{
-    console.log(monthlyIncomes[revenue as keyof Incomes2])
-}
-
-//3:01:54
+// 3:12:28
